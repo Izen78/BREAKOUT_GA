e54e0a9085ce2ff0b45b6eb176914f6217f00941
Arpon Sarker <izenhoe@gmail.com>
2025-07-07 14:13:08 +1000

    Finished rendering red brick as a row

--- main.py
+++ main.py
@@ -1,14 +1,65 @@
+# TODO: Deltatime
+# TODO: Render all bricks
+
 import pygame
 import os
+from typing import Type
 
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 400
-# Import Images
+TOPLEFT_CORNER = (33, 60)
+BRICK_TOPLEFT_CORNER= (0, -10)
 
+# Import Images
 i_bg = pygame.image.load("imgs/bg.png")
 i_one = pygame.image.load("imgs/one.png")
+i_rb = pygame.image.load("imgs/hot_dog.png")
+i_ob = pygame.image.load("imgs/donut.png")
+i_br_b = pygame.image.load("imgs/cookie.png")
+i_yb = pygame.image.load("imgs/burrito.png")
+i_gb = pygame.image.load("imgs/jelly.png")
+i_bb = pygame.image.load("imgs/swiss_roll.png")
+
 class Brick:
-    pass
+    score_val = None
+    alive = True
+    img = None
+
+    def __init__(self, score_val) -> None:
+        self.score_val = score_val
+
+    def collision(self, ball: type) -> bool:
+        pass
+
+class Red_Brick(Brick):
+    score_val = 100
+    img = i_rb
+    
+    def __init__(self) -> None:
+        super().__init__(self.score_val)
+
+class Orange_Brick(Brick):
+    score_val = 75 
+    img = i_rb
+    
+    def __init__(self) -> None:
+        super().__init__(self.score_val)
+
+class Brown_Brick(Brick):
+    score_val = 50
+
+class Yellow_Brick(Brick):
+    score_val = 25
+
+class Green_Brick(Brick):
+    score_val = 10
+
+class Blue_Brick(Brick):
+    score_val = 5
+
+
+
+
 
 class Ball:
     pass
@@ -21,7 +72,15 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+# Initial Screen
 screen.blit(i_bg, (0, 0))
+
+# 18 red bricks across (12x32) first brick starts at x=32, y=64
+rbs = []
+for i in range(18):
+    rbs.append(Red_Brick())
+for i, rb in enumerate(rbs):
+    screen.blit(rb.img, ((33+i*rb.img.get_width(),TOPLEFT_CORNER[1]-4*BRICK_TOPLEFT_CORNER[1])))
 
 while running:
     # poll for events
